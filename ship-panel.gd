0543eb25c0f264c00e8e44ca7b79225a3af8235c
20ml <milliliter.avc@gmail.com>
2025-03-01 14:22:15 -0500

    Create basic ship display

---
+++ ship-panel.gd
@@ -0,0 +1,44 @@
+extends Panel
+
+@export var ship: Ship
+
+@export var current_z_layer : int = 0
+
+@onready var grid_holder : Control = $"%GRID-HOLDER"
+@onready var grid_square : TextureRect = grid_holder.get_child(0)
+
+func _ready():
+	grid_holder.remove_child(grid_square)
+
+const grid_size : int = 32
+
+func _input(event):
+	if event is InputEventMouse:
+		var ev_pos : Vector2 = event.position
+		var grid_space_pos : Vector2 = ev_pos - grid_holder.global_position
+		var grid_pos : Vector2i = grid_space_pos / grid_size
+		print("%s | %s | %s" % [ev_pos, grid_space_pos, grid_pos])
+	elif event is InputEventKey:
+		if event.pressed and Input.is_key_pressed(KEY_SHIFT):
+			if event.keycode == KEY_W:
+				current_z_layer += 1
+			elif event.keycode == KEY_S:
+				current_z_layer -= 1
+
+func _process(_delta):
+	for child in grid_holder.get_children():
+		child.queue_free()
+
+	var hull_size : Vector2i = Vector2i.ZERO
+
+	for voxel_position : Vector3i in ship.hull_structure.hull_voxels:
+		hull_size = hull_size.max(Vector2i(voxel_position.x, voxel_position.y))
+		if voxel_position.z != current_z_layer: continue
+		var grid_square_instance = grid_square.duplicate()
+		grid_square_instance.custom_minimum_size = Vector2i.ONE * grid_size
+		grid_square_instance.size = Vector2i.ONE * grid_size
+		grid_square_instance.position = Vector2(voxel_position.x, voxel_position.y) * grid_size
+		grid_holder.add_child(grid_square_instance)
+
+	grid_holder.custom_minimum_size = hull_size * grid_size
+	
