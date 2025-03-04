cbd29dde74e7cf34abaafb7953cd6d83293c767e
20ml <milliliter.avc@gmail.com>
2025-03-04 12:46:09 -0500

    Sysinfo view update via eventbus

---
+++ src/autoload/ProjectHammerEventBus.gd
@@ -0,0 +1,52 @@
+#class_name ProjectHammerEventBus
+extends Node
+
+var subscriptions : Dictionary = {} #Dictionary[StringName, Array[Callable]]
+var mediators : Dictionary = {} #Dictionary[StringName, Callable]
+
+func subscribe(event_name : StringName, return_address : Callable) -> void:
+	if !subscriptions.has(event_name): 
+		var new : Array[Callable] = []
+		subscriptions[event_name] = new
+
+	var event_subscribers : Array[Callable] = subscriptions[event_name]
+	event_subscribers.append(return_address)
+	#ProjectHammerLogger.log(["EVENT"], "Object %s subscribed to event %s" % [return_address.get_object(), event_name])
+	return
+
+func unsubscribe(event_name : StringName, return_address : Callable) -> void:
+	if !subscriptions.has(event_name): 
+		return
+	
+	var event_subscribers : Array[Callable] = subscriptions[event_name]
+	event_subscribers.erase(return_address)
+	#ProjectHammerLogger.log(["EVENT"], "Object %s unsubscribed from event %s" % [return_address.get_object(), event_name])
+	return
+
+func mediate(event_name : StringName, return_address : Callable) -> void:
+	if mediators.has(event_name):
+		#ProjectHammerLogger.error("ERROR: Recieved request to mediate event \"%s\", but it already has a mediator." % [event_name])
+		return
+		
+	mediators[event_name] = return_address
+	#ProjectHammerLogger.log(["EVENT"], "Object %s now mediates event '%s'" % [return_address.get_object(), event_name])
+	return
+
+func push_events(data : Dictionary = {}) -> void:
+	for event_name : StringName in data.keys():
+		var single_event : Dictionary = data[event_name]
+		push_event(event_name, single_event)
+
+func push_event(event_name : StringName, data : Dictionary = {}) -> void:
+	var event_subscribers : Array[Callable] = []
+	if subscriptions.has(event_name):
+		event_subscribers = subscriptions[event_name]
+		for event_subscriber : Callable in event_subscribers:
+			if mediators.has(event_name):
+				var mediator : Callable = mediators[event_name]
+				event_subscriber.call(mediator.call(data))
+			else:
+				event_subscriber.call(data)
+	#if not event_name.contains("log_message") and not event_name.contains("error_message"):
+		##ProjectHammerLogger.log(["EVENT"], "Event '%s' pushed to %s subscribers" % [event_name, len(event_subscribers)])
+	return
--- src/ui/UISystemsTree.gd
+++ src/ui/UISystemsTree.gd
@@ -7,13 +7,16 @@ var _inst_system_to_tree_item : Dictionary = {} #[InstalledSystem, UISystemsTree
 func _ready():
 	tree_item_template.visible = false
 	update_tree()
+	
+	ProjectHammerEventBus.subscribe("ship/installed_systems_changed", update_tree)
 
-func update_tree():
+func update_tree(_data : Dictionary = {}):
 	for inst_system in ShipManager.ship.installed_systems:
 		if not _inst_system_to_tree_item.has(inst_system):
 			var sys_item : UISystemsTreeItem = tree_item_template.duplicate()
 			tree_item_template.add_sibling(sys_item)
 			sys_item.visible = true
-			sys_item.set_header_text(inst_system.system.name)
+			var sys_pos : Vector3i = inst_system.region.to_AABB().get_center()
+			sys_item.set_header_text("%s @ <%s>" %[inst_system.system.name, str(sys_pos).left(-1).right(-1)])
 			_inst_system_to_tree_item[inst_system] = sys_item
 			sys_item.set_system_info(inst_system)
