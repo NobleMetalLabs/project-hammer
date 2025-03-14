9cb61764f467e254be80d03e7b9b26e3e0618264
20ml <milliliter.avc@gmail.com>
2025-03-14 18:26:40 -0400

    wip

--- lib/character/Character.gd
+++ lib/character/Character.gd
@@ -3,4 +3,5 @@ extends Resource
 
 var name : String = ""
 var faction : FactionDB.Faction
+var ethics : Ethics = Ethics.new()
 var wallet : Wallet = Wallet.new()
---
+++ tst/ethics_test.gd
@@ -0,0 +1,5 @@
+extends Control
+
+var current_character : Character = null
+var current_choice : EthicalChoice = null
+
