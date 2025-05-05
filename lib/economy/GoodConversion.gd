c3202b8816fae91ef424ce200ab72be9b32af881
20ml <milliliter.avc@gmail.com>
2025-05-05 18:40:39 -0400

    items, good conversions

---
+++ lib/economy/GoodConversion.gd
@@ -0,0 +1,5 @@
+class_name GoodConversion
+extends Resource
+
+@export var inputs : Dictionary = {} # [ItemDB.Item, float]
+@export var outputs : Dictionary = {} # [ItemDB.Item, float]
--- lib/economy/Producer.gd
+++ lib/economy/Producer.gd
@@ -2,7 +2,3 @@ class_name Producer
 extends Node
 
 @export var production_status : ProductionStatus
-
-func _ready() -> void:
-	production_status.ingredient_requirements[ItemDB.Item.ORE] = 4
-	production_status.product_goals[ItemDB.Item.CONSTRUCTION_MATERIALS] = 7
